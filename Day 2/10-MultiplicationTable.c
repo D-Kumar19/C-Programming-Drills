61ad1582705ed8045e1480312d0b79bd07910f50
Dheeraj Kumar <dheerajkumar.190102@gmail.com>
2022-07-06 12:14:35 +0200

    Add files via upload

--- Day 2/10-MultiplicationTable.c
+++ Day 2/10-MultiplicationTable.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+void printTablePositiveLimit(int number, int limit){
+    for(int i = 1; i <= limit ; i++){
+        printf("%d   *   %d   =   %d\n", number, i, number*i);
+    }
+}
+
+void printTableNegativeLimit(int number, int limit){
+    for(int i = -1; i >= limit ; i--){
+        printf("%d   *   %d   =   %d\n", number, i, number*i);
+    }
+}
+
 int main(){
     
     printf("This program will print Multiplication Table for the Limit and Number given!\n");
@@ -10,8 +22,12 @@ int main(){
     printf("Enter the Limit of the Multiplication Table: ");
     scanf("%d", &limit);
 
-    for(int i = 1; i <= limit ; i++){
-        printf("%d * %d = %d\n", number, i, number*i);
+    printf("Multiplication Table!\n");
+    if(limit >= 0){
+        printTablePositiveLimit(number, limit);
+    }
+    else{
+        printTableNegativeLimit(number, limit);
     }
     
     return 0;
